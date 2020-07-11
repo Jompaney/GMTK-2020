05b4f99b3f83f74a2868cc6cc940f2ce6b7098ed
Jompaney <64639310+Jompaney@users.noreply.github.com>
2020-07-11 17:28:23 +0200

    some basic stuff

---
+++ Out Of Control - GameJam/Assets/Movement.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed;
+    public TimeManager TimeManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Intersection"))
+        {
+            Debug.Log("Intersection");
+            TimeManager.SlowMotion();
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Intersection"))
+        {
+            TimeManager.StopSlowMotion();
+        }
+    }
+}
