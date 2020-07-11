56a3ac32eeaf077200fa098b4eaa965cb57a733d
XanJan <annamajberger@gmail.com>
2020-07-11 22:57:26 +0200

    Fixat:)

--- Out Of Control - GameJam/Assets/Movement.cs
+++ Out Of Control - GameJam/Assets/Movement.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Movement : MonoBehaviour
 {
@@ -10,11 +12,22 @@ public class Movement : MonoBehaviour
     List<KeyCode> randomFour;
     bool intersectionPhase = false;
     private bool HasTurned = false;
+    public TextMeshProUGUI LeftText;
+    public TextMeshProUGUI RightText;
+    public TextMeshProUGUI UpText;
+    private string LeftBinding;
+    private string RightBinding;
+    private string UpBinding;
+    private Vector3 intersectionPos;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        LeftText = GameObject.Find("Text Left").GetComponent<TextMeshProUGUI>();
+        RightText = GameObject.Find("Text Right").GetComponent<TextMeshProUGUI>();
+        UpText = GameObject.Find("Text Up").GetComponent<TextMeshProUGUI>();
+
         randomFour = new List<KeyCode> { };
     }
 
@@ -22,21 +35,29 @@ public class Movement : MonoBehaviour
     void Update()
     {
         transform.Translate(Vector3.forward * movementSpeed * Time.deltaTime);
-        if ((intersectionPhase)&&(!HasTurned))
+        if ((intersectionPhase) && (!HasTurned))
         {
             TurnDirection();
+
         }
-        
+
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
+
         if (other.CompareTag("Intersection"))
         {
+            intersectionPos = other.transform.position;
             intersectionPhase = true;
             KeyBinds();
             Debug.Log("Intersection");
             TimeManager.SlowMotion();
+
+            LeftText.text = LeftBinding;
+            RightText.text = RightBinding;
+            UpText.text = UpBinding;
         }
         foreach (var item in randomFour)
         {
@@ -56,35 +77,49 @@ public class Movement : MonoBehaviour
 
     void KeyBinds()
     {
-        while (randomFour.Count<3)
+        while (randomFour.Count < 3)
         {
-            var index = Random.Range(0, Keys.Count-1);
-            if(!randomFour.Contains(Keys[index]))
+            var index = Random.Range(0, Keys.Count - 1);
+            if (!randomFour.Contains(Keys[index]))
             {
                 randomFour.Add(Keys[index]);
             }
         }
+        LeftBinding = randomFour[0].ToString();
+        RightBinding = randomFour[1].ToString();
+        UpBinding = randomFour[2].ToString();
     }
 
     void TurnDirection()
     {
-        if(Input.GetKeyDown(randomFour[0]))
+        if (Input.GetKeyDown(randomFour[0]))
         {
             Debug.Log("111");
-            transform.Rotate(Vector3.up, 90);
+            transform.Rotate(Vector3.up, -90);
             HasTurned = true;
+
+            TimeManager.StopSlowMotion();
+            intersectionPhase = false;
+            transform.position = intersectionPos;
         }
         if (Input.GetKey(randomFour[1]))
         {
             Debug.Log("222");
+
+            TimeManager.StopSlowMotion();
+            intersectionPhase = false;
+            transform.position = intersectionPos;
         }
         if (Input.GetKey(randomFour[2]))
         {
             Debug.Log("ww333ww");
+            transform.Rotate(Vector3.up, 90);
+            HasTurned = true;
+
+            TimeManager.StopSlowMotion();
+            intersectionPhase = false;
+            transform.position = intersectionPos;
         }
-        //if (Input.GetKey(randomFour[3]))
-        //{
-        //    Debug.Log("444");
-        //}
+
     }
 }
