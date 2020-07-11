517660b7fe3bad894e40025bd797d6ee25d89793
XanJan <annamajberger@gmail.com>
2020-07-11 22:53:53 +0200

    Revert "Player Movment etc"
    This reverts commit 01e1ab655c7e1d148684c5c5feebe51cd2e64f28.

--- Out Of Control - GameJam/Assets/Movement.cs
+++ Out Of Control - GameJam/Assets/Movement.cs
@@ -1,8 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
-using TMPro;
 
 public class Movement : MonoBehaviour
 {
@@ -12,22 +10,11 @@ public class Movement : MonoBehaviour
     List<KeyCode> randomFour;
     bool intersectionPhase = false;
     private bool HasTurned = false;
-    public TextMeshProUGUI LeftText;
-    public TextMeshProUGUI RightText;
-    public TextMeshProUGUI UpText;
-    private string LeftBinding;
-    private string RightBinding;
-    private string UpBinding;
-    private Vector3 intersectionPos;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        LeftText = GameObject.Find("Text Left").GetComponent<TextMeshProUGUI>();
-        RightText = GameObject.Find("Text Right").GetComponent<TextMeshProUGUI>();
-        UpText = GameObject.Find("Text Up").GetComponent<TextMeshProUGUI>();
-
         randomFour = new List<KeyCode> { };
     }
 
@@ -38,26 +25,18 @@ public class Movement : MonoBehaviour
         if ((intersectionPhase)&&(!HasTurned))
         {
             TurnDirection();
-           
         }
-        
         
     }
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.CompareTag("Intersection"))
         {
-            intersectionPos = other.transform.position;
             intersectionPhase = true;
             KeyBinds();
             Debug.Log("Intersection");
             TimeManager.SlowMotion();
-
-            LeftText.text = LeftBinding;
-            RightText.text = RightBinding;
-            UpText.text = UpBinding;
         }
         foreach (var item in randomFour)
         {
@@ -85,9 +64,6 @@ public class Movement : MonoBehaviour
                 randomFour.Add(Keys[index]);
             }
         }
-        LeftBinding = randomFour[0].ToString();
-        RightBinding = randomFour[1].ToString();
-        UpBinding = randomFour[2].ToString();
     }
 
     void TurnDirection()
@@ -95,31 +71,20 @@ public class Movement : MonoBehaviour
         if(Input.GetKeyDown(randomFour[0]))
         {
             Debug.Log("111");
-            transform.Rotate(Vector3.up, -90);
+            transform.Rotate(Vector3.up, 90);
             HasTurned = true;
-
-            TimeManager.StopSlowMotion();
-            intersectionPhase = false;
-            transform.position = intersectionPos;
         }
         if (Input.GetKey(randomFour[1]))
         {
             Debug.Log("222");
-
-            TimeManager.StopSlowMotion();
-            intersectionPhase = false;
-            transform.position = intersectionPos;
         }
         if (Input.GetKey(randomFour[2]))
         {
             Debug.Log("ww333ww");
-            transform.Rotate(Vector3.up, 90);
-            HasTurned = true;
-
-            TimeManager.StopSlowMotion();
-            intersectionPhase = false;
-            transform.position = intersectionPos;
         }
-       
+        //if (Input.GetKey(randomFour[3]))
+        //{
+        //    Debug.Log("444");
+        //}
     }
 }
