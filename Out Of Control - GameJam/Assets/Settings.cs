3b96e1ff592a9f7b4f17a07cb06dfc18b9c20880
XanJan <annamajberger@gmail.com>
2020-07-12 17:18:53 +0200

    hej

--- Out Of Control - GameJam/Assets/Settings.cs
+++ Out Of Control - GameJam/Assets/Settings.cs
@@ -1,11 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEngine.Audio;
 using UnityEngine;
 
 public class Settings : MonoBehaviour
 {
+    public AudioMixer audioMixer;
     public void SetVolume(float volume)
     {
-
+        audioMixer.SetFloat("Volume", volume);
     }
 }
